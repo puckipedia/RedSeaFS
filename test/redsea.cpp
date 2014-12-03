f0c7d3a332e5c4717e4d62f8fd14a2c43fab4f5e
Puck Meerburg <puck@puckipedia.nl>
2014-12-03 19:59:58 +0100

    Add allocation code

--- test/redsea.cpp
+++ test/redsea.cpp
@@ -24,12 +24,16 @@ public:
 				RedSea(FILE *f);
 	RedSeaDirectory *	RootDirectory();
 	uint64_t		BaseOffset() { return mBoot.base_offset; }
+	uint64_t		FirstFreeSector(int count);
+	uint64_t		Allocate(int count);
 private:
 	friend class RedSeaDirEntry;
 	friend class RedSeaFile;
 	friend class RedSeaDirectory;
 	FILE *mFile;
 	RSBoot mBoot;
+	uint8_t *mBitmapSectors;
+	uint64_t mBitmapLength;
 	void			Read(uint64_t location, uint64_t count, void *result);
 };
 
@@ -37,6 +41,91 @@ RedSea::RedSea(FILE *f)
 {
 	mFile = f;
 	Read(0, 0x200, &mBoot);
+	mBitmapLength = mBoot.bitmap_sectors * 0x200;
+	mBitmapSectors = new uint8_t[mBitmapLength];
+	Read(0x200, mBitmapLength * 0x200, mBitmapSectors);
+}
+
+//#define UINT64_MAX 0xFFFFFFFFFFFFFFFF
+
+uint64_t
+RedSea::FirstFreeSector(int count)
+{
+	//printf("Finding %d sectors next to each other...\n");
+	int i;
+	uint64_t start = UINT64_MAX;
+	int ccount = 0;
+	for (i = 0; i < mBitmapLength; i++) {
+		int bi, ci;
+		for (bi = 1, ci = 0; ci < 8; bi <<= 1, ci++) {
+			if (!(mBitmapSectors[i] & bi)) {
+				//printf("Found free sector 0x%016X-%d\n", i, ci);
+				if (start == UINT64_MAX) {
+					//printf("\tStart updated\n");
+					start = (i << 3) | ci;
+				}
+				ccount++;
+				if (ccount >= count)
+					return start;
+				//printf("Count is now %d\n", ccount);
+			} else {
+				//printf("Full sector, 0x%016X-%d - %d > %d?\n", i, ci, ccount, count);
+				if (ccount >= count)
+					return start;
+				start = ~0;
+			}
+		}
+	}
+
+	if (ccount >= count)
+		return start;
+
+	return ~0;
+}
+
+uint64_t
+RedSea::Allocate(int count)
+{
+	uint64_t firstFree = FirstFreeSector(count);
+	if (firstFree == UINT64_MAX)
+		return firstFree;
+
+	uint64_t start = firstFree;
+	uint64_t end = firstFree + count - 1; // inclusive!
+
+	int startbit = start % 8;
+	int endbit = end % 8;
+
+	start >>= 3;
+	end >>= 3;
+
+	if (start == end) {
+		for (int i = startbit; i <= endbit; i++) {
+			printf("Set 0x%016X %d to 1\n", start, i);
+			mBitmapSectors[start] |= (1 << i);
+		}
+	} else {
+		if (startbit == 0) {
+			mBitmapSectors[start] = 0xFF;
+		} else {
+			for (int i = startbit; i < 8; i++) {
+				mBitmapSectors[start] |= 1 << i;
+			}
+		}
+
+		if (endbit == 0) {
+			memset(mBitmapSectors + start + 1, 0xFF, end - start + 1);
+		} else {
+			memset(mBitmapSectors + start + 1, 0xFF, end - start);
+			for (int i = 0; i <= endbit; i++) {
+				mBitmapSectors[end] |= 1 << i;
+			}
+		}
+
+		return firstFree;
+	}
+
+	return firstFree;
 }
 
 void
@@ -179,6 +268,9 @@ RedSea::RootDirectory()
 void print(RedSeaDirectory *dir, int indent = 0) {
 	for (int i = 0; i < dir->CountEntries(); i++) {
 		RedSeaDirEntry *entry = dir->GetEntry(i);
+		if (entry == 0)
+			continue;
+
 		if (strcmp(entry->GetName(), "..") == 0)
 			printf("%.*sSpecial dir '%s'\n", indent, "\t", entry->GetName());
 		else if (entry->IsDirectory()) {
@@ -203,5 +295,13 @@ int main(int argc, char **argv)
 
 	print(d, 0);
 
+	int i;
+	srand(1234);
+	for (i = 0; i < 10; i++) {
+		int ij = rand() % 1234;
+		printf("Allocating %d sector(s): \n", ij);
+		uint64_t alloc = r.Allocate(ij);
+		printf("\tGot 0x%016X\n", alloc);
+	}
 	return 0;
 }
